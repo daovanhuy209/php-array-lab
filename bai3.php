8fd33046f20d7fbbc4119081d497e73ec356c584
daovanhuy209 <daovanhuy2513@gmail.com>
2026-03-09 07:24:38 +0700

    Create bai3.php

---
+++ bai3.php
@@ -0,0 +1,23 @@
+<?php
+
+function timGiaTri($giaTri,$mang){
+
+    for($i = 0; $i < count($mang); $i++){
+
+        if($mang[$i] == $giaTri){
+            return $i;
+        }
+
+    }
+
+    return -1;
+
+}
+
+$traiCay = ["Apple","Banana","Cherry"];
+
+$index = timGiaTri("Banna",$traiCay);
+
+echo $index;
+
+?>
